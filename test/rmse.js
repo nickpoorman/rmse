51182cc61ee592f2202da41a54e6949201aa2325
Nick Poorman <nickpoorman@gmail.com>
2014-11-24 11:40:54 -0500

    added tests

--- test/rmse.js
+++ test/rmse.js
@@ -3,7 +3,7 @@ var RMSE = require('../');
 
 describe('RMSE', function() {
 
-  it('should produce a valid RMSE score', function() {
+  it('should produce a valid mean square error', function() {
 
     var dataset = [{
       actual: 3,
@@ -20,5 +20,55 @@ describe('RMSE', function() {
     }];
 
     RMSE.mse(dataset).should.equal(0.375);
+  });
+
+  it('should produce a valid root mean square error', function() {
+
+    var dataset = [{
+      actual: 0.5,
+      predicted: 0
+    }, {
+      actual: 1,
+      predicted: 2
+    }, {
+      actual: -1,
+      predicted: -1
+    }, {
+      actual: 1,
+      predicted: 2
+    }, {
+      actual: 7,
+      predicted: 8
+    }, {
+      actual: -6,
+      predicted: -5
+    }];
+
+    parseFloat(RMSE.mse(dataset).toPrecision(3)).should.equal(0.708);
+  });
+
+  it('should produce a valid root mean square error', function() {
+
+    var dataset = [{
+      actual: 0.5,
+      predicted: 0
+    }, {
+      actual: 1,
+      predicted: 2
+    }, {
+      actual: -1,
+      predicted: -1
+    }, {
+      actual: 1,
+      predicted: 2
+    }, {
+      actual: 7,
+      predicted: 8
+    }, {
+      actual: -6,
+      predicted: -5
+    }];
+
+    parseFloat(RMSE.rmse(dataset).toPrecision(3)).should.equal(0.842);
   });
 });
